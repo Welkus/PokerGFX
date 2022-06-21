33e85e70a6da07d80fdec09cc11165957b90828c
Krzysiek <k_woelke@o2.pl>
2022-06-21 11:05:00 +0200

    Gui nowa wersja 112 po usunięciu pozostałych 4 graczy

--- src/main/java/client/TableController.java
+++ src/main/java/client/TableController.java
@@ -11,14 +11,14 @@ public class TableController{
 
     @FXML public TextField p1;
     @FXML public TextField p2;
-    @FXML public TextField p3;
-    @FXML public TextField p4;
-    @FXML public TextField p5;
+//    @FXML public TextField p3;
+//    @FXML public TextField p4;
+//    @FXML public TextField p5;
     @FXML public TextField c1;
     @FXML public TextField c2;
-    @FXML public TextField c3;
-    @FXML public TextField c4;
-    @FXML public TextField c5;
+//    @FXML public TextField c3;
+//    @FXML public TextField c4;
+//    @FXML public TextField c5;
     @FXML private TextField raisebox;
     @FXML public TextField whichPturn;
 
@@ -27,43 +27,43 @@ public class TableController{
 
     public Circle circle;
     public Circle circle2;
-    public Circle circle3;
-    public Circle circle4;
-    public Circle circle5;
+//    public Circle circle3;
+//    public Circle circle4;
+//    public Circle circle5;
     public Circle pot1;
     public Circle pot2;
-    public Circle pot3;
-    public Circle pot4;
-    public Circle pot5;
-    public Circle pot6;
+//    public Circle pot3;
+//    public Circle pot4;
+//    public Circle pot5;
+//    public Circle pot6;
     public Circle avatar1;
     public Circle avatar2;
-    public Circle avatar3;
-    public Circle avatar4;
-    public Circle avatar5;
+//    public Circle avatar3;
+//    public Circle avatar4;
+//    public Circle avatar5;
 
     @FXML public ImageView cc1;
     @FXML public ImageView cc2;
-    @FXML public ImageView cc3;
-    @FXML public ImageView cc4;
-    @FXML public ImageView cc5;
+//    @FXML public ImageView cc3;
+//    @FXML public ImageView cc4;
+//    @FXML public ImageView cc5;
     @FXML public ImageView card21;
     @FXML public ImageView card22;
-    @FXML public ImageView card31;
-    @FXML public ImageView card32;
-    @FXML public ImageView card41;
-    @FXML public ImageView card42;
-    @FXML public ImageView card51;
-    @FXML public ImageView card52;
+//    @FXML public ImageView card31;
+//    @FXML public ImageView card32;
+//    @FXML public ImageView card41;
+//    @FXML public ImageView card42;
+//    @FXML public ImageView card51;
+//    @FXML public ImageView card52;
 
     @FXML public Text bet;
     @FXML public Text action;
     @FXML public Text pot;
     @FXML public Text message;
     @FXML public Text action2;
-    @FXML public Text action3;
-    @FXML public Text action4;
-    @FXML public Text action5;
+//    @FXML public Text action3;
+//    @FXML public Text action4;
+//    @FXML public Text action5;
 
     public void call(){
 
@@ -77,10 +77,10 @@ public class TableController{
                       Main.client.selfBet = Main.client.currentbet;
                       Main.client.isTurn = false;
                   }
-                  else message.setText("You can't call now. Check, raise or fold.");
+                  else message.setText("Nie moższ teraz sprawdzić. Wybierz Podbij lub Rzucam.");
               }
               else {
-                  message.setText("You need to fold now!"); // today
+                  message.setText("Musisz rzucić karty!"); // today
 
                   Main.client.out.println("fold");
                   Main.client.isTurn = false;
@@ -88,7 +88,7 @@ public class TableController{
                  // card2.setImage(new Image("resources/download.jpg"));
               }
           }
-          else message.setText("Not Your Turn!");
+          else message.setText("Nie Twoja kolej!");
       }
 
     public void check(){
@@ -100,9 +100,9 @@ public class TableController{
                   Main.client.out.println("check");
                   Main.client.isTurn = false;
               }
-              else message.setText("You need to call the current bet!");
+              else message.setText("Musisz sprawdzić aktualny zakład!");
           }
-          else message.setText("Not Your Turn!");
+          else message.setText("Nie Twoja kolej!");
     }
 
     public void raise() {
@@ -121,14 +121,14 @@ public class TableController{
                         raisebox.clear();
                         Main.client.isTurn = false;
                     }
-                    else message.setText("You can't raise lower than current bet");
+                    else message.setText("Nie możesz postawić mniej, niż aktualna wartość zakładu.");
                 }
-                else message.setText("Not Enough Chips!");
+                else message.setText("Za mało chipów!");
             }
-            else message.setText("Not Your Turn!");
+            else message.setText("Nie Twoja kolej!");
 
         }catch (NumberFormatException e){
-            message.setText("Please input Numbers");
+            message.setText("Należy wprowadzić liczby");
         }
     }
 
